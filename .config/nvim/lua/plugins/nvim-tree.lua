1a881c63774b62bb55055da08ae9f3aae6dc7c1e
Ethan Byeon <ethanbyeon+github@gmail.com>
2024-11-03 16:36:54 -0800

    Show git ignored files in tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,61 +1,64 @@
 local function setup_tree()
-    local view = {
-        side = "left",
-        number = true,
-        width = 35,
-        relativenumber = true,
-    }
+	local view = {
+		side = "left",
+		number = true,
+		width = 35,
+		relativenumber = true,
+	}
 
-    local renderer = {
-        indent_markers = {
-            enable = true,
-            icons = {
-                corner = "└",
-                edge = "│",
-                item = "│",
-                bottom = "─",
-                none = " ",
-            },
-        },
-        icons = {
-            webdev_colors = true,
-            git_placement = "after",
-            modified_placement = "after",
-            show = {
-                file = true,
-                folder = true,
-                folder_arrow = false,
-                git = true,
-                modified = true,
-            },
-            glyphs = {
-                git = {
-                    unstaged = "",
-                    staged = "S",
-                    unmerged = "",
-                    renamed = "➜",
-                    untracked = "U",
-                    deleted = "D",
-                    ignored = "◌",
-                },
-            },
-        },
-    }
+	local renderer = {
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				bottom = "─",
+				none = " ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+			git_placement = "after",
+			modified_placement = "after",
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = false,
+				git = true,
+				modified = true,
+			},
+			glyphs = {
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "U",
+					deleted = "D",
+					ignored = "◌",
+				},
+			},
+		},
+	}
 
-    require("nvim-tree").setup({
-        on_attach = map_on_attach,
-        view = view,
-        renderer = renderer,
-        update_focused_file = {
-            update_root = true,
-        },
-    })
+	require("nvim-tree").setup({
+		on_attach = map_on_attach,
+		view = view,
+		renderer = renderer,
+		update_focused_file = {
+			update_root = true,
+		},
+		git = {
+			ignore = false,
+		},
+	})
 end
 
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = setup_tree,
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = setup_tree,
 }
